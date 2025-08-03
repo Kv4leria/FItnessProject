a420edc6e961e4451a6caa7cb828bf766cd9efd0
k4rina <floresk87@gator.uhd.edu>
2025-08-03 17:31:58 -0500

    created a FitnessProfile structure to be used when user registers

---
+++ FItnessProject/FitnessProfile.h
@@ -0,0 +1,20 @@
+#ifndef FITNESSPROFILE_H
+#define FITNESSPROFILE_H
+
+#include <string>
+using namespace std;
+
+struct FitnessProfile {
+
+	double currentweight; 
+	double goalWeight;
+	double height;
+	int age;
+	string gender; 
+	string activityLevel;
+
+};
+
+
+#endif // !FitnessProfile
+
