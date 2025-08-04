687228f9f35c4406667f945d8cbe81bcb7c669d6
k4rina <floresk87@gator.uhd.edu>
2025-08-04 00:17:33 -0500

    started Finess Menu

--- FItnessProject/mainMenu.cpp
+++ FItnessProject/mainMenu.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include "User.h"
 using namespace std; 
 
 int main() {
+	 
+	int choice;
+	do {
 
-	cout << "hello" << endl;
+		cout << "\n -- Fitness Menu-- \n";
+		cout << "1. Register" << endl;
+		cout << "2. Login as User" << endl;
+		cout << "3. Login as a Coach" << endl;
+		cout << "4. Exit" << endl;
+		cin >> choice;
 
+		//if (choice == 1)
 
-
+	} while (choice != 4);
 
 	return (0);
 }
