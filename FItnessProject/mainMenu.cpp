a08b8b1c4b5baaaf6d24a3d71f9b9fd95827bfa7
merari424 <merariescobedo.424@gmail.com>
2025-08-06 19:00:09 -0500

    Save Karina's code

--- FItnessProject/mainMenu.cpp
+++ FItnessProject/mainMenu.cpp
@@ -3,59 +3,13 @@
 #include "User.h"
 #include "FitnessProfile.h"
 #include "coachDashboard.h"
-using namespace std; 
+using namespace std;  
 
-vector <User> users; // All registeres users 
-
-void registerUser(coachDashboard& dashboard) {
-
-	string username;
-	string password;
-	FitnessProfile profile;
-
-	cout << "--- Registration ---" << endl; 
-	cout << "Username: ";
-	cin >> username;
-	cout << "Password: ";
-	cin >> password;
-
-	cout << "Current weight: ";
-	cin >> profile.currentweight;
-
-	cout << "Goal weight:  ";
-	cin >> profile.goalWeight;
-
-	cout << "Height in cm: ";
-	cin >> profile.height;
-
-	cout << "Age: ";
-	cin >> profile.height;
-
-	cout << "Gender: ";
-	cin >> profile.gender;
-
-	cout << "Activity Level (Light / Moderate / Heavy (Athlete) ): ";
-	cin >> profile.activityLevel;
-
-	User newUser(username, password, profile);
-	users.push_back(newUser);
-	dashboard.addClientFromUser(users.back());
-
-	cout << "Registration successful! You've been assigned to a coach!" << endl;
-
-
-}
-
+vector <User> users;
 
 int main() {
 
-	FitnessProfile coachProfile{ 0.0,0.0,0.0,0,"", "" };
-	User demoCoach("demoCoach", "123", coachProfile);
-	coachDashboard dashboard(demoCoach);
-
 	int choice;
-
-
 	do {
 
 		cout << "\n -- Fitness Menu-- \n";
@@ -65,18 +19,15 @@ int main() {
 		cout << "4. Exit" << endl;
 		cin >> choice;
 
-		if (choice == 1) {
+		//if (choice == 1)
 		
-			registerUser(dashboard);
-
-		}
-		
-	//else if
+		//else if (choice == 
 
 		/*else*/ if (choice == 3) {
 			string username, password;
 
-		
+			FitnessProfile coachProfile{ 0.0,0.0,0.0,0,"", "" };
+			User demoCoach("demoCoach", "123", coachProfile);
 
 			cout << "------Login------" << endl;
 			cout << "Username: ";
